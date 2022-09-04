533ca96256bc0d49502fe139eeef65d358e4bea9
Pavel <Casear4000@gmail.com>
2022-09-04 14:39:15 +0300

    HW03: fix

--- hw03/junior/fibonacci.go
+++ hw03/junior/fibonacci.go
@@ -1,16 +1,21 @@
 package main
 
 func Fibonacci(n int) int {
-	a := 0
-	b := 1
+	first := 0
+	second := 1
 
-	for i := 0; i < n; i++ {
-		temp := a
-		a = b
-		b = temp + a
+	if n == 0 {
+		return first
+	} else if n <= 2 {
+		return second
 	}
 
-	return a
+	for i := 2; i <= n; i++ {
+		second = second + first
+		first = second - first
+	}
+
+	return second
 }
 
 func FibonacciRecursive(n int) int {
--- hw03/junior/main.go
+++ hw03/junior/main.go
@@ -7,10 +7,12 @@ func main() {
 	fmt.Printf("%d^%d is %d === 256\n\n", 2, 8, Power(2, 8))
 
 	fmt.Printf("%dth number of Fibonacci is %d === 8\n", 6, Fibonacci(6))
-	fmt.Printf("%dth number of Fibonacci is %d === 144\n\n", 12, FibonacciRecursive(12))
+	fmt.Printf("%dth number of Fibonacci is %d === 144\n", 12, Fibonacci(12))
+	fmt.Printf("%dth number of Fibonacci is %d === 46368\n\n", 24, Fibonacci(24))
 
 	fmt.Printf("[recursive] %dth number of Fibonacci is %d === 8\n", 6, FibonacciRecursive(6))
-	fmt.Printf("[recursive] %dth number of Fibonacci is %d === 144\n\n", 12, FibonacciRecursive(12))
+	fmt.Printf("[recursive] %dth number of Fibonacci is %d === 144\n", 12, FibonacciRecursive(12))
+	fmt.Printf("[recursive] %dth number of Fibonacci is %d === 46368\n\n", 24, FibonacciRecursive(24))
 
 	fmt.Printf("PrimeCount of %d is %d === 4\n", 10, PrimeCount(10))
 	fmt.Printf("PrimeCount of %d is %d === 25\n\n", 100, PrimeCount(100))
